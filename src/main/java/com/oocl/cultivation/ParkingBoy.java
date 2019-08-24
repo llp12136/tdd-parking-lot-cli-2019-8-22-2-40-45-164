cab585067af0fda4f3b947f77776df21c945e2ee
llp12136 <liliping@zybank.com.cn>
2019-08-24 17:52:25 +0800

    park0824

--- src/main/java/com/oocl/cultivation/ParkingBoy.java
+++ src/main/java/com/oocl/cultivation/ParkingBoy.java
@@ -1,8 +1,9 @@
 package com.oocl.cultivation;
 
 public class ParkingBoy {
-    private ParkingLot parkingLot;
+    private final ParkingLot parkingLot;
 
+    private String lastErrorMessage ;
     public ParkingBoy(ParkingLot parkingLot) {
         // TODO Auto-generated constructor stub
         this.parkingLot = parkingLot;
@@ -10,10 +11,31 @@ public class ParkingBoy {
 
     public ParkingTicket park(Car car) {
         // TODO Auto-generated method stub
+
+       if(parkingLot.parkCar(car) != null) {
+            lastErrorMessage = null;
+        }
+     else{lastErrorMessage="The parking lot is full.";}
         return parkingLot.parkCar(car);
     }
 
     public Car fetch(ParkingTicket ticket) {
-        return parkingLot.pickCar(ticket);
+       Car  getCar =parkingLot.pickCar(ticket);
+        if(ticket == null){
+            lastErrorMessage = "Please provide your parking ticket.";
+         }
+        else if(getCar == null) {
+            lastErrorMessage = "Unrecognized parking ticket.";
+        }
+
+    return   getCar;
+
+    }
+
+    public void setLastErrorMessage(String lastErrorMessage) {
+        this.lastErrorMessage = lastErrorMessage;
+    }
+    public String getLastErrorMessage() {
+        return lastErrorMessage;
     }
 }
--- src/main/java/com/oocl/cultivation/ParkingLot.java
+++ src/main/java/com/oocl/cultivation/ParkingLot.java
@@ -4,18 +4,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ParkingLot {
-    private Car car;
+   // private Car car;
    // Private ParkingTicket ticket;
+   private final int capacity;
+    private Map<ParkingTicket, Car> cars = new HashMap<>();
+
+    public ParkingLot() {
+        this(10);
+    }
+
+    public ParkingLot(int capacity) {
+        this.capacity = capacity;
+    }
+
 
     public ParkingTicket parkCar(Car car) {
-        this.car = car;
-        return new ParkingTicket();
+        ParkingTicket ticket;
+       if(cars.size()>=capacity) {
+           ticket = null;
+       }
+       else {
+        ParkingTicket newTicket = new ParkingTicket();
+        ticket = newTicket;
+        cars.put(ticket, car);}
+        return ticket;
     }
 
     public Car pickCar(ParkingTicket ticket) {
-        // TODO Auto-generated method stub
-       // this.ticket = ticket;
+       Car car = cars.get(ticket);
+        cars.remove(ticket);
         return car;
-    }
 
+    }
+    public int getAvailableParkingPosition() {
+        return cars.size() - capacity;
+    }
 }
